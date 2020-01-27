f47c73a817aca6b274f321b9478d1e65cbc77b4f
rob pickard <rob.pickard11@gmail.com>
2020-01-27 17:58:55 +0000

    Implicit grant auth flow implemented

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const AuthorizeButton = (props) => (
+  <button
+    onClick={props.handleClick}
+  >
+    Authorize
+  </button>
+)
+
 const App = () => {
 
+  // Add state to auth flow for security
+  const baseAuthUrl = 'https://accounts.spotify.com/authorize?'
+  const redirectUri = 'http://localhost:3000/' // Change for production
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+
+  const authorize = () => {
+    window.location= `${baseAuthUrl}client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}`
+  }
+
   return (
-    <div></div>
+    <div>
+      <AuthorizeButton
+        handleClick={authorize}
+      />
+    </div>
   )
 }
 
