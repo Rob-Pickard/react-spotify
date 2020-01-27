795af594f450d6b6f15267d95ebf74fc0e50506d
rob pickard <rob.pickard11@gmail.com>
2020-01-27 18:41:54 +0000

    Added effect hook to check for auth token on app render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -11,20 +11,34 @@ const AuthorizeButton = (props) => (
 )
 
 const App = () => {
+  const [ token, setToken ] = useState(false)
 
   // Add state to auth flow for security
   const baseAuthUrl = 'https://accounts.spotify.com/authorize?'
   const redirectUri = 'http://localhost:3000/' // Change for production
   const clientId = process.env.REACT_APP_CLIENT_ID;
 
-  const authorize = () => {
-    window.location= `${baseAuthUrl}client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}`
+  // Check for token on app render
+  useEffect(() => {
+    const hash = window.location.hash.substring(1)
+    const params = {}
+    hash.split('&').forEach(param => {
+      let keyValue = param.split('=');
+      params[keyValue[0]] = keyValue[1]
+    });
+    if(params.access_token !== undefined) {
+      setToken(params.access_token)
+    }
+  }, [])
+
+  const authorizeRedirect = () => {
+    window.location = `${baseAuthUrl}client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}`
   }
 
   return (
     <div>
       <AuthorizeButton
-        handleClick={authorize}
+        handleClick={authorizeRedirect}
       />
     </div>
   )
