63691b59073d6397c8e716ae614ef8f73addbf23
rob pickard <rob.pickard11@gmail.com>
2020-01-29 18:08:27 +0000

    Landing page styled

--- src/components/authorize_panel.js
+++ src/components/authorize_panel.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import spotifyLogo from '../assets/spotify-logo.png';
+import reactLogo from '../assets/react-logo.png';
 
-const AuthorizePanel = (props) => (
-  <button
-    onClick={props.handleClick}
-  >
-    Log in with Spotify
-  </button>
+const AuthorizeCover = (props) => (
+  <div className="login-background">
+    <div className="login-button-container">
+      <button
+        onClick={props.handleClick}
+        className="login-button"
+      >
+        Log in with Spotify
+      </button>
+    </div>
+    <div className="login-page-image-container">
+      <img
+        src={spotifyLogo}
+        alt="Spotify logo"
+        className="login-tech-logo-spotify"
+      />
+      <img
+        src={reactLogo}
+        alt="React JS logo"
+        className="login-tech-logo-react"
+      />
+    </div>
+  </div>
 )
 
-export default AuthorizePanel
+export default AuthorizeCover
--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import authService from './services/authorization'
 import spotifyService from './services/spotify'
-import AuthorizePanel from './components/authorize_panel.js'
+import AuthorizeCover from './components/authorize_panel.js'
 import PlayingConditional from './components/player.js'
 import './stylesheets/index.scss';
 
 const AuthConditional = (props) => {
   if(props.authorized) {
     return (
-      <PlayingConditional
-        updatePlaybackData={props.updatePlaybackData}
-        playbackData={props.playbackData}
-        handlePlayPauseButtonClick={props.handlePlayPauseButtonClick}
-        handleNextButtonClick={props.handleNextButtonClick}
-        handlePrevButtonClick={props.handlePrevButtonClick}
-      />
+      <div className="center-media-wrapper">
+        <PlayingConditional
+          updatePlaybackData={props.updatePlaybackData}
+          playbackData={props.playbackData}
+          handlePlayPauseButtonClick={props.handlePlayPauseButtonClick}
+          handleNextButtonClick={props.handleNextButtonClick}
+          handlePrevButtonClick={props.handlePrevButtonClick}
+        />
+      </div>
     )
   }
   return (
-    <AuthorizePanel
+    <AuthorizeCover
       handleClick={props.handleAuthButtonClick}
     />
   )
@@ -63,17 +65,15 @@ const App = () => {
   }
 
   return (
-    <div className="center-media-wrapper">
-      <AuthConditional
-        authorized={token ? true : false}
-        handleAuthButtonClick={authService.authorizeRedirect}
-        updatePlaybackData={updatePlaybackData}
-        playbackData={playbackData}
-        handlePlayPauseButtonClick={togglePlay}
-        handleNextButtonClick={() => adjacentTrack("next")}
-        handlePrevButtonClick={() => adjacentTrack("previous")}
-      />
-    </div>
+    <AuthConditional
+      authorized={token ? true : false}
+      handleAuthButtonClick={authService.authorizeRedirect}
+      updatePlaybackData={updatePlaybackData}
+      playbackData={playbackData}
+      handlePlayPauseButtonClick={togglePlay}
+      handleNextButtonClick={() => adjacentTrack("next")}
+      handlePrevButtonClick={() => adjacentTrack("previous")}
+    />
   )
 }
 
